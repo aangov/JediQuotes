9532ce45036b6fa8ae676a95783952b10ac26e87
Arangel Angov <arangel@linux.net.mk>
2012-04-14 22:26:07 +0200

    adding files

---
+++ jediquotes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+
+import twitter
+import random
+import ConfigParser
+
+config=ConfigParser.ConfigParser()
+config.read("config")
+credentials=dict()
+for key, value in config.items("Credentials"):
+	credentials[key]=value
+
+print credentials
+
+f = open ("jediquotes", "r")
+
+data = []
+	
+for line in f.readlines():
+	data.append(line)
+
+api = twitter.Api(**credentials)
+
+api.PostUpdate(random.choice(data))
